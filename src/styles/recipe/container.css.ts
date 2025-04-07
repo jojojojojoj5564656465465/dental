2136acc59af50f70721507eb142cb7b6a3adeca3
jonathan <bcbg92@hotmail.com>
2025-04-07 13:05:31 +0200

    sprinkles in recipe

--- src/styles/recipe/container.css.ts
+++ src/styles/recipe/container.css.ts
@@ -1,73 +1,85 @@
-import { recipe } from '@vanilla-extract/recipes'
-import { fallbackVar } from '@vanilla-extract/css'
-import { colorTheme, theme } from '../utils/themeNew.css.ts'
-import { maxInlineSizeFn } from '@styles/variants/base.css.ts'
+import {recipe} from '@vanilla-extract/recipes'
+import {fallbackVar, globalStyle} from '@vanilla-extract/css'
+import {colorTheme, theme} from '../utils/themeNew.css.ts'
+import {maxInlineSizeFn} from '@styles/variants/base.css.ts'
 
-const { accent, darkBlueBanner, blueLightBg, whiteBg } = colorTheme
 
 export const container = recipe({
-  base: {
-    position: 'relative',
-    boxSizing: 'border-box',
-    color: theme.text,
-    marginInline: 'auto',
-  },
-  variants: {
-    theme: {
-      accent,
-      darkBlueBanner,
-      whiteBg,
-      blueLightBg,
+    base: {
+        position: 'relative',
+        boxSizing: 'border-box',
+        color: theme.text,
+        marginInline: 'auto',
     },
-
-    hover: {
-      true: {
-        cursor: 'pointer',
-        ':active': {
-          color: fallbackVar(theme.textHover, 'inherit'),
-          backgroundColor: fallbackVar(theme.backgroundHover, 'inherit'),
-          transform: 'scale(1.01,1)',
-          outline: `min(4px, 3px + 0.1vw) solid ${theme.backgroundHover}`,
-          outlineOffset: '1.6px',
-        },
-        ':focus': {
-          outline: 'min(4px, 3px + 0.1vw) solid yellow',
-          outlineOffset: '4px',
+    variants: {
+        theme: {
+            ...colorTheme
         },
 
-        ':hover': {
-          backgroundColor: fallbackVar(theme.backgroundHover, theme.background),
-          color: fallbackVar(theme.textHover, 'inherit'),
-          borderColor: fallbackVar(theme.textHover, 'inherit'),
+        hover: {
+            true: {
+                cursor: 'pointer',
+                ':active': {
+                    color: fallbackVar(theme.textHover, 'inherit'),
+                    backgroundColor: fallbackVar(theme.backgroundHover, 'inherit'),
+                    transform: 'scale(1.01,1)',
+                    outline: `min(4px, 3px + 0.1vw) solid ${theme.backgroundHover}`,
+                    outlineOffset: '1.6px',
+                },
+                ':focus': {
+                    outline: 'min(4px, 3px + 0.1vw) solid yellow',
+                    outlineOffset: '4px',
+                },
+
+                ':hover': {
+                    backgroundColor: fallbackVar(theme.backgroundHover, theme.background),
+                    color: fallbackVar(theme.textHover, theme.text, 'inherit'),
+                    borderColor: fallbackVar(theme.textHover, 'inherit'),
+                },
+            },
         },
-      },
+        background: {
+            true: {
+                backgroundColor: theme.background,
+            },
+        },
+        size: {
+            small: {
+                maxInlineSize: maxInlineSizeFn('small'),
+            },
+            medium: {
+                maxInlineSize: maxInlineSizeFn('medium'),
+            },
+            large: {
+                maxInlineSize: maxInlineSizeFn('large'),
+            },
+            xxl: {
+                maxInlineSize: maxInlineSizeFn('xxl'),
+            },
+            full: {
+                maxInlineSize: 'none',
+            },
+        },
     },
-    background: {
-      true: {
-        backgroundColor: theme.background,
-      },
+    defaultVariants: {
+        hover: false,
+        background: true,
+        size: 'full',
     },
-    size: {
-      small: {
-        maxInlineSize: maxInlineSizeFn('small'),
-      },
-      medium: {
-        maxInlineSize: maxInlineSizeFn('medium'),
-      },
-      large: {
-        maxInlineSize: maxInlineSizeFn('large'),
-      },
-      xxl: {
-        maxInlineSize: maxInlineSizeFn('xxl'),
-      },
-      full: {
-        maxInlineSize: 'none',
-      },
+})
+
+globalStyle(
+    `${container.classNames.variants.size.small}:has(> :is(${container.classNames.variants.size.medium}, ${container.classNames.variants.size.large},${container.classNames.variants.size.xxl},)) > *`,
+    {
+        maxInlineSize: '100%',
     },
-  },
-  defaultVariants: {
-    hover: false,
-    background: true,
-    size: 'full',
-  },
+)
+globalStyle(
+    `${container.classNames.variants.size.medium}:has(> :is(${container.classNames.variants.size.large}, ${container.classNames.variants.size.xxl})) > *`,
+    {
+        maxInlineSize: '100%',
+    },
+)
+globalStyle(`${container.classNames.variants.size.large}:has(> :is(${container.classNames.variants.size.xxl})) > *`, {
+    maxInlineSize: '100%',
 })
