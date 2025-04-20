9c5bb0fe2b7ef6fec06e3357c9c88389f3e2f68f
jonathan <bcbg92@hotmail.com>
2025-04-21 01:46:31 +0200

    hello

--- src/components/HOME/1-HERO/hero.css.ts
+++ src/components/HOME/1-HERO/hero.css.ts
@@ -78,7 +78,7 @@ export const banner_hr = style({
     [media.mobile]: {
       display: 'none',
     },
-    ['all and ( width <= 641px)']: {
+    ['all and ( width <= 640px)']: {
       display: 'none',
     },
   },
