23624e658c38334fbec9e1b6765799aca1b8ea0e
jonathan <bcbg92@hotmail.com>
2025-05-04 02:10:54 +0200

    update but not finish navbar3 90%

--- src/components/Layout-Component/Navbar3/submenu.tsx
+++ src/components/Layout-Component/Navbar3/submenu.tsx
@@ -1,5 +1,5 @@
 // src/components/Navbar3/submenu.tsx
-import { $, component$, type Signal, useContext, useSignal, useStyles$ } from '@builder.io/qwik'
+import { $, component$, useComputed$, useContext, useSignal, useStyles$ } from '@builder.io/qwik'
 import * as v from 'valibot'
 import { openMenuIndexPosition } from './index'
 import styles from './main.css?inline'
@@ -27,8 +27,8 @@ export default component$<SubMenuB & Extra>(props => {
   v.parse(ObjectSchema, props)
 
   const openSubMenu = useSignal<boolean>(false)
-  const theme = useContext(openMenuIndexPosition)
-
+  const ctx = useContext(openMenuIndexPosition)
+  const openCompute = useComputed$(() => props.idx === ctx.value)
   const toggleSubMenu = $(() => {
     openSubMenu.value = !openSubMenu.value
   })
@@ -39,13 +39,13 @@ export default component$<SubMenuB & Extra>(props => {
       <button
         type={'button'}
         onclick$={[toggleSubMenu, $(() => props.changeTab(props.idx))]}
-        onMouseEnter$={toggleSubMenu}
+        onMouseEnter$={[toggleSubMenu, $(() => props.changeTab(props.idx))]}
         onMouseLeave$={toggleSubMenu}
-        class={['submenu_name cursor-pointer text-xl!', openSubMenu.value ? 'text-green-500' : 'text-red-600 ']}
+        class={['submenu_name cursor-pointer text-xl!', openCompute.value ? 'text-green-500' : 'text-red-600 ']}
       >
         {props.name} {props.idx}
       </button>
-      <ul class={['submenu_navlink gap-y-1 flex-col', openSubMenu.value ? 'flex' : 'hidden']}>
+      <ul class={['submenu_navlink gap-y-1 flex-col', openCompute.value ? 'flex' : 'hidden']}>
         {props.Submenu.map((item, index) => (
           <Navlink key={index} {...item} />
         ))}
