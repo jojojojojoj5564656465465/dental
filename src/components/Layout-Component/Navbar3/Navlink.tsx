f2afb67acd01e404d119f69c21997b1ceada1e9a
jonathan <bcbg92@hotmail.com>
2025-05-06 02:46:10 +0200

    update but not finish navbar3 94%

--- src/components/Layout-Component/Navbar3/Navlink.tsx
+++ src/components/Layout-Component/Navbar3/Navlink.tsx
@@ -7,7 +7,7 @@ import type { A } from './menu'
 export default component$<A>(props => {
   useStyles$(styles)
   return (
-    <li class=' flex justify-center items-center group/nvl navlink navStyle flex hover:bg-sky-500 focus:bg-sky-500 active:bg-sky-500 cursor-pointer text-center md:w-max '>
+    <li class='flex justify-center items-center group/nvl navlink navStyle flex hover:bg-sky-500 focus:bg-sky-500 active:bg-sky-500 cursor-pointer text-center md:w-max '>
       <a class={'group-hover/nvl:text-white mx-auto'} href={props.link}>
         {props.name}
       </a>
